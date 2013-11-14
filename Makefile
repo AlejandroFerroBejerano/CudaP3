d41e3b983ae58d0361359c1f9f19c2c43ea5e4e4
Alejandro Ferro Bejerano <alejandroferro.1988@gmail.com>
2013-11-14 22:54:11 +0100

    Añadido regla modificado histograma_multiblocks

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 
-all: histograma
+all: histograma histograma_multiblocks
 
 histograma: histograma.cu
 	    nvcc -arch=sm_20 $ -o $@ $^
+
+histograma_multiblocks: histograma_multiblocks.cu
+	    		nvcc -arch=sm_20 $ -o $@ $^
 
 clean:
 	$(RM) *~ *.o histograma
--- histograma.cu
+++ histograma.cu
@@ -8,12 +8,12 @@
 
 #define RangoColores 256
 #define Nbloques 1
-#define NThreads 256 
+#define NThreads 256
 
 __global__ void histograma_kernel(unsigned char *buffer, long size, unsigned int *hist){
 	
 		/*Buffer de histograma temporal en memoria compartida*/	
-	__shared__ unsigned int temp[256];
+	__shared__ unsigned int temp[RangoColores];
 	temp[threadIdx.x]=0;
 	__syncthreads();
 
@@ -44,22 +44,22 @@ int main(void){
 	/*Calculamos la longitud máxima en memoria que ocupa la imagen*/
 	long size = IMAGE_WIDTH * IMAGE_HEIGHT;
 	
-	
+	/*Reservamos memoria e inicializamos a 0 
+	todo el rango donde se almacenara el histograma*/
 	cudaMalloc((void**) &dev_image, size);
 	cudaMalloc((void**) &dev_histograma, RangoColores * sizeof(int));
 	cudaMemset( dev_histograma, 0,RangoColores * sizeof( int ) );
 
 	cudaMemcpy(dev_image, img, size, cudaMemcpyHostToDevice);
-	
 	histograma_kernel<<<Nbloques,NThreads>>>(dev_image,size,dev_histograma);
-
 	cudaMemcpy(histograma, dev_histograma, RangoColores * sizeof(int), cudaMemcpyDeviceToHost);
-
+	
+	/*Comprobamos uqe el cálculo se ha hecho correctamente haciendo
+	la operación inversa con la CPU*/
 	for (int i=0; i<size; i++) histograma[img[i]]--;
 	for(int i=0; i< RangoColores; i++){
 		if (histograma[i] !=0) printf("Error en %d  Valor %d\n ", i, histograma[i]);
 	}
-
 	
 	cudaFree(dev_image);
 	cudaFree(dev_histograma);
